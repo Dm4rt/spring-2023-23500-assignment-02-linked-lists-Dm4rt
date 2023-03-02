65e620eb712eeeda4179c2d131b8c02a511e9da0
Dm4rt <danmartinez2004@gmail.com>
2023-03-02 10:36:17 -0500

    Part 1

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
   l->insert(1,"b");
   l->insert(2,"c");
   l->insert(3,"d");
+  l->insert(4,"f");
   std::cout << l->toString() << " " << l->length() << "\n";
   try {
     l->insert(10,"x");
@@ -17,6 +18,9 @@ int main()
     std::cout <<"Insert x didn't work\n";
   }
  std::cout<<"1 is true and 0 is false \n Contains c ="<<l->contains("c")<<std::endl;
+  std::cout<<"Contains f ="<<l->contains("f")<<std::endl;
+  l->remove(4);
+  std::cout<<"removing f"<<std::endl;
  std::cout<<"Contains f ="<<l->contains("f")<<std::endl;
   delete l;
 
