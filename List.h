d8aed5267c80a5580ba4646cc131bb28b0c5a6f5
Dm4rt <danmartinez2004@gmail.com>
2023-03-02 10:33:42 -0500

    Part 1

---
+++ List.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include "Node.h"
+
+class List{
+ private:
+  Node *head;
+ public:
+  List();
+  ~List();
+  void insert(std::string data);
+  void insert(int loc, std::string data); //0 indexed
+  int length();
+  std::string find(std::string data);
+  bool contains(std::string item);
+  std::string toString(); // for testing purposes
+  void remove(int loc);
+
+  /*
+    
+    
+  */
+  
+  
+
+};
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "List.h"
+
+int main()
+{
+  List *l = new List();
+  std::cout << l->toString() << " " << l->length() << "\n";
+  l->insert(0,"a");
+  std::cout << l->toString() << " " << l->length() << "\n";
+  l->insert(1,"b");
+  l->insert(2,"c");
+  l->insert(3,"d");
+  std::cout << l->toString() << " " << l->length() << "\n";
+  try {
+    l->insert(10,"x");
+  } catch (std::exception e){
+    std::cout <<"Insert x didn't work\n";
+  }
+ std::cout<<"1 is true and 0 is false \n Contains c ="<<l->contains("c")<<std::endl;
+ std::cout<<"Contains f ="<<l->contains("f")<<std::endl;
+  delete l;
+
+
+  return 0;
+}
